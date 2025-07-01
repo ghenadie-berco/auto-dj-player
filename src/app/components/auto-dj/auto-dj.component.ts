ccd78af210d32d73aca63083aa81f2f3581bf66f
Ghenadie Berco <berco.ghenadie@fortis-cs.com>
2025-07-01 13:28:20 -0400

    cleanup

--- src/app/components/auto-dj/auto-dj.component.ts
+++ src/app/components/auto-dj/auto-dj.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { PlaylistComponent } from './components/playlist/playlist.component';
-import { AudioPlayerComponent } from './components/audio-player/audio-player.component';
 
 @Component({
   selector: 'app-auto-dj',
   templateUrl: './auto-dj.component.html',
   styleUrl: './auto-dj.component.scss',
   imports: [
-    PlaylistComponent,
-    AudioPlayerComponent,
+    PlaylistComponent
   ],
 })
 export class AutoDjComponent {
