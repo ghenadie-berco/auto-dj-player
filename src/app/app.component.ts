c3f5493e9788e3509d787b4f400b748d71b1cf51
Ghenadie Berco <berco.ghenadie@fortis-cs.com>
2025-07-01 12:55:08 -0400

    project cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,10 @@
-import { Component, viewChild } from '@angular/core';
-import { AudioTrackPlayerComponent } from './components/audio-track-player/audio-track-player.component';
-import { TwoChannelMixerComponent } from './components/two-channel-mixer/two-channel-mixer.component';
-import { PlaylistComponent } from './components/playlist/playlist.component';
-import { Song } from './app.interfaces';
+import { Component } from '@angular/core';
+import { AutoDjComponent } from './components/auto-dj/auto-dj.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [
-    AudioTrackPlayerComponent,
-    TwoChannelMixerComponent,
-    PlaylistComponent,
-  ],
+  imports: [AutoDjComponent],
 })
-export class AppComponent {
-  public title = 'web';
-
-  public playerARef = viewChild<AudioTrackPlayerComponent>('playerA');
-  public playerBRef = viewChild<AudioTrackPlayerComponent>('playerB');
-  public mixerRef = viewChild<TwoChannelMixerComponent>('mixer');
-
-  public onLoadToDeckA(song: Song) {
-    const deckAVolume = this.mixerRef()?.getLeftChannelVolume() ?? 0;
-    this.playerARef()?.loadNewTrack(song, deckAVolume);
-  }
-
-  public onLoadToDeckB(song: Song) {
-    const deckBVolume = this.mixerRef()?.getRightChannelVolume() ?? 0;
-    this.playerBRef()?.loadNewTrack(song, deckBVolume);
-  }
-
-  public onMixerLeftChannelVolumeChange(volume: number) {
-    this.playerARef()?.setVolume(volume);
-  }
-
-  public onMixerRightChannelVolumeChange(volume: number) {
-    this.playerBRef()?.setVolume(volume);
-  }
-
-  public onFadeTimeReachedDeckA() {
-    console.log('Fade time reached for Deck A');
-  }
-
-  public onFadeTimeReachedDeckB() {
-    console.log('Fade time reached for Deck B');
-  }
-}
+export class AppComponent {}
