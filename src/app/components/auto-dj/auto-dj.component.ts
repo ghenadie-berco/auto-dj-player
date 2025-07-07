e2064007324414b6a21e96f3d0ae79955036a64b
Ghenadie Berco <berco.ghenadie@fortis-cs.com>
2025-07-06 20:45:07 -0400

    wip

--- src/app/components/auto-dj/auto-dj.component.ts
+++ src/app/components/auto-dj/auto-dj.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { PlaybackComponent } from './components/playback/playback.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { ControlsComponent } from './components/controls/controls.component';
 
 @Component({
   selector: 'app-auto-dj',
-  imports: [],
   templateUrl: './auto-dj.component.html',
-  styleUrl: './auto-dj.component.scss'
+  styleUrl: './auto-dj.component.scss',
+  host: { class: 'component flex-col' },
+  imports: [
+    PlaybackComponent,
+    PlaylistComponent,
+    ControlsComponent,
+  ],
 })
 export class AutoDjComponent {
 
---
+++ src/app/components/auto-dj/components/playback/playback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playback',
+  imports: [],
+  templateUrl: './playback.component.html',
+  styleUrl: './playback.component.scss'
+})
+export class PlaybackComponent {
+
+}
