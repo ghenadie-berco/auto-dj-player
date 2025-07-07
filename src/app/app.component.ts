df4d9dab1987619151a7db7fb07098419f169daf
Ghenadie Berco <berco.ghenadie@fortis-cs.com>
2025-07-06 20:30:21 -0400

    design by first HIFI prototype - wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { AutoDjComponent } from './components/auto-dj/auto-dj.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [],
+  host: { class: 'view flex-col' },
+  imports: [
+    HeaderComponent,
+    AutoDjComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {}
---
+++ src/app/components/auto-dj/auto-dj.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-dj',
+  imports: [],
+  templateUrl: './auto-dj.component.html',
+  styleUrl: './auto-dj.component.scss'
+})
+export class AutoDjComponent {
+
+}
